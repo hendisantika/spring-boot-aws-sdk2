ad1a4e7c90e384927668a0e5d51e1a7506c22503
hendisantika <hendisantika@yahoo.co.id>
2024-11-07 11:43:59 +0700

    Add s3AsyncClient in AmazonS3Config🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/awssdk2/config/AmazonS3Config.java
+++ src/main/java/id/my/hendisantika/awssdk2/config/AmazonS3Config.java
@@ -1,7 +1,11 @@
 package id.my.hendisantika.awssdk2.config;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +22,13 @@ import org.springframework.context.annotation.Configuration;
 public class AmazonS3Config {
 
     private final AwsProperties awsProperties;
+
+    @Bean("s3AsyncClient")
+    public S3AsyncClient s3AsyncClient(@Qualifier("awsCredentials") AwsCredentialsProvider awsCredentials) {
+        return S3AsyncClient
+                .builder()
+                .credentialsProvider(awsCredentials)
+                .region(Region.of(awsProperties.getS3().getRegion()))
+                .build();
+    }
 }
