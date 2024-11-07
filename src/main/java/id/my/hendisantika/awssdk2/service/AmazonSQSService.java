905a898743c0663e9374fdc6527a0fcd2a4de935
hendisantika <hendisantika@yahoo.co.id>
2024-11-07 11:53:08 +0700

    Add consume in AmazonSQSService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/awssdk2/service/AmazonSQSService.java
+++ src/main/java/id/my/hendisantika/awssdk2/service/AmazonSQSService.java
@@ -1,10 +1,20 @@
 package id.my.hendisantika.awssdk2.service;
 
 import id.my.hendisantika.awssdk2.config.AwsProperties;
+import id.my.hendisantika.awssdk2.utils.MyTuple;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchRequestEntry;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageBatchResponse;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+
+import java.util.Collections;
+import java.util.List;
+
+import static id.my.hendisantika.awssdk2.utils.AwsUtils.getSQSQueueUrl;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +34,30 @@ public class AmazonSQSService {
     private final SqsClient sqsClient;
     private final AwsProperties awsProperties;
 
+    public MyTuple.MyTuple2<List<Message>, DeleteMessageBatchResponse> consume() {
+        ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest.builder()
+                .queueUrl(getSQSQueueUrl(awsProperties))
+                .waitTimeSeconds(awsProperties.getSqs().getWaitTime())
+                .visibilityTimeout(awsProperties.getSqs().getVisibilityTimeout())
+                .maxNumberOfMessages(awsProperties.getSqs().getMaxMessage())
+                .build();
+
+        List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+        if (messages.isEmpty()) return new MyTuple.MyTuple2<>(Collections.emptyList(), null);
+
+        List<DeleteMessageBatchRequestEntry> entries = messages.stream()
+                .map(message -> DeleteMessageBatchRequestEntry.builder()
+                        .id(message.messageId())
+                        .receiptHandle(message.receiptHandle())
+                        .build())
+                .toList();
+
+        DeleteMessageBatchRequest deleteMessageBatchRequest = DeleteMessageBatchRequest.builder()
+                .queueUrl(getSQSQueueUrl(awsProperties))
+                .entries(entries)
+                .build();
+
+        DeleteMessageBatchResponse deleteMessageBatchResponse = sqsClient.deleteMessageBatch(deleteMessageBatchRequest);
+        return new MyTuple.MyTuple2<>(messages, deleteMessageBatchResponse);
+    }
 }
