e2a4f56aed4bd814c3f2bd0a4d8ac0201ecedb6f
hendisantika <hendisantika@yahoo.co.id>
2024-11-07 11:55:25 +0700

    Add updateFile in AmazonS3Controller🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/awssdk2/controller/AmazonS3Controller.java
+++ src/main/java/id/my/hendisantika/awssdk2/controller/AmazonS3Controller.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +47,10 @@ public class AmazonS3Controller {
         return multipartFiles.stream()
                 .map(amazonS3Service::uploadFile)
                 .collect(Collectors.toList());
+    }
+
+    @PutMapping
+    public String updateFile(@RequestParam String fileName, @RequestParam("file") MultipartFile multipartFile) {
+        return amazonS3Service.updateFile(fileName, multipartFile);
     }
 }
