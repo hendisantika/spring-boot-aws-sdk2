8a5bf1f5dd5b841e4e13375d9db77241cd9699f1
hendisantika <hendisantika@yahoo.co.id>
2024-11-07 11:59:41 +0700

    fix: Error🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/awssdk2/config/AmazonS3Config.java
+++ src/main/java/id/my/hendisantika/awssdk2/config/AmazonS3Config.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
 
 /**
  * Created by IntelliJ IDEA.
